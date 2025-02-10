402ccd94900529fd79981c7216d25f1dfbed7962
Rishabh Jain <rishabh.jain@CJDARCL.CORP>
2025-02-10 14:34:30 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+// import { SquareComponent } from './components/square/square.component';
+// import { BoardComponent } from './components/board/board.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+// import { GetClassPipe } from './pipes/get-class.pipe';
+import { TaskFormComponent } from './task-form/task-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    // SquareComponent,
+    // BoardComponent,
+    // GetClassPipe,
+    TaskFormComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
+
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
